7c7c1169c521437281b6ddd736acbed3c59d484c
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-04-23 16:14:04 +1200

    Fixed the bug where I forgot ;

--- FinanceWebsite/Expense.cs
+++ FinanceWebsite/Expense.cs
@@ -1,7 +1,7 @@
 namespace FinanceWebsite;
 
 public class Expense(){
-    public string? Name {get; set}
+    public string? Name {get; set;}
     public int Amount {get; set;}
     public bool IsAfterpay {get; set;} = false;
 }
